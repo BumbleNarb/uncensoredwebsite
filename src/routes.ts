06806e2fd58c8fef3992adac57389469fcb820d7
BumbleNarb <brandonyhchan@gmail.com>
2026-04-09 05:21:16 +0800

    Fixed routing

--- src/routes.ts
+++ src/routes.ts
@@ -1,31 +1,24 @@
 import { createBrowserRouter } from "react-router";
-import { Home } from "./pages/Home";
+import { AdminPage } from "./pages/AdminPage";
 import { CategoryPage } from "./pages/CategoryPage";
 import { CheckoutPage } from "./pages/CheckoutPage";
-import { AdminPage } from "./pages/AdminPage";
-import { Layout } from "./components/Layout";
+import { TemporaryPage } from "./pages/temporary/TemporaryPage";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: Layout,
-    children: [
-      {
-        index: true,
-        Component: Home,
-      },
-      {
-        path: "category/:categoryName",
-        Component: CategoryPage,
-      },
-      {
-        path: "checkout",
-        Component: CheckoutPage,
-      },
-      {
-        path: "admin",
-        Component: AdminPage,
-      },
-    ],
+    Component: TemporaryPage,
+  },
+  {
+    path: "category/:categoryName",
+    Component: CategoryPage,
+  },
+  {
+    path: "checkout",
+    Component: CheckoutPage,
+  },
+  {
+    path: "admin",
+    Component: AdminPage,
   },
 ]);
